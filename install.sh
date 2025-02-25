b97a9a06286ffb6592792a23d373d1e1b25f7402
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-02-25 19:28:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# // Warna
+#// Warna
 red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
 NC='\e[0m'
 
-# // Buat Direktori Yang Diperlukan
 cd /root
 mkdir -p /etc/xray
+mkdir -p /etc/ssnvpn
+mkdir -p /etc/ssnvpn/theme
+mkdir -p /var/lib/ssnvpn-pro >/dev/null 2>&1
 # SET DOMAIN
 echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
     echo -e "${red}    ♦️${NC} ${green} CUSTOM SETUP DOMAIN VPS     ${NC}"
@@ -20,13 +22,10 @@ echo -e "${red}━━━━━━━━━━━━━━━━━━━━━�
     echo "$domen" > /root/scdomain
     echo "$domen" > /etc/xray/domain
     echo "$domen" > /etc/xray/scdomain
-    echo "IP=$domen" > /var/lib/ssnvpn-pro/ipvps.conf
     cp /root/domain /etc/xray/domain
     clear
- echo "IP=" >> /var/lib/ssnvpn-pro/ipvps.conf
- clear
  
-#System version number
+# // Cek Root
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
@@ -36,6 +35,7 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		exit 1
 fi
 
+# // Ambil Informasi VPS
 localip=$(hostname -I | cut -d\  -f1)
 hst=( `hostname` )
 dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
@@ -43,13 +43,12 @@ if [[ "$hst" != "$dart" ]]; then
 echo "$localip $(hostname)" >> /etc/hosts
 fi
 
-secs_to_human() {
-    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
-}
+# // Set Local Time GMT +7
 start=$(date +%s)
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+
 
 coreselect=''
 cat> /root/.profile << END
